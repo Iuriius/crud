84eb4ece91e0c421a3f1ba5be5f8c822efe67e48
Iurii Kyrychenko <105295809+Iuriius@users.noreply.github.com>
2023-12-11 21:32:24 +0200

    1

--- components/EditBtn.tsx
+++ components/EditBtn.tsx
@@ -1,9 +1,13 @@
 import Link from "next/link";
 import EditIcon from "./icons/EditIcon";
 
-export default function EditBtn() {
-    return (
-        <Link href={`/editTopic/${topic._id}`}>
-            <button className="btn btn-outline btn-warning"><EditIcon />Edit Topic</button></Link>
-    )
+export default function EditBtn({ topic }) {
+  return (
+    <Link href={`/editTopic/${topic._id}`}>
+      <button className="btn btn-outline btn-warning">
+        <EditIcon />
+        Edit Topic
+      </button>
+    </Link>
+  );
 }
--- components/TopicsList.tsx
+++ components/TopicsList.tsx
@@ -2,38 +2,43 @@ import EditBtn from "./EditBtn";
 import RemoveBtn from "./RemoveBtn";
 
 const getTopics = async () => {
-    try {
-        const response = await fetch("http://localhost:3000/api/topics")
-        cache: "no-store";
-        if (!response.ok) {
-            throw new Error("Failed to fetch data")
-        }
-        return response.json();
+  try {
+    const response = await fetch("http://localhost:3000/api/topics", {
+      cache: "no-store",
+    });
 
-    } catch (error) {
-        console.log("Error loading Topics:", error);
-
+    if (!response.ok) {
+      throw new Error("Failed to fetch data");
     }
-}
-
+    return response.json();
+  } catch (error) {
+    console.log("Error loading Topics:", error);
+  }
+};
 
 export default async function TopicsList() {
+  const { topics } = await getTopics();
 
-    const { topics } = await getTopics();
-
-    return (
+  return (
     <>
-            {topics.map((topic) => (
-                < div className="my-4 p-4 border border-slate-300 rounded-lg flex justify-between items-start" >
-                    <div className="max-w-md">
-                        <h2 className="font-bold text-2xl text-white p-2">{topic.title}</h2>
-                        <p className="p-2">{topic.description}
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat assumenda nostrum culpa expedita vero atque nulla sit temporibus, est dolorem ut velit aliquid omnis laborum hic excepturi alias dolores vitae!
-                        </p>
-                    </div>
-                    <div className="flex gap-2 flex-col">
-                        <EditBtn />
-                        <RemoveBtn />
-                    </div>))}
-                </>
-            )}
+      {topics.map((topic: any) => (
+        <div className="my-4 p-4 border border-slate-300 rounded-lg flex justify-between items-start">
+          <div className="max-w-md">
+            <h2 className="font-bold text-2xl text-white p-2">{topic.title}</h2>
+            <p className="p-2">
+              {topic.description}
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat
+              assumenda nostrum culpa expedita vero atque nulla sit temporibus,
+              est dolorem ut velit aliquid omnis laborum hic excepturi alias
+              dolores vitae!
+            </p>
+          </div>
+          <div className="flex gap-2 flex-col">
+            <EditBtn topic={topic} />
+            <RemoveBtn />
+          </div>
+        </div>
+      ))}
+    </>
+  );
+}
