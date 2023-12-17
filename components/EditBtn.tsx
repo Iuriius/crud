25ff773014da5d6fadba44a18eb5ef96e514610d
Iurii Kyrychenko <105295809+Iuriius@users.noreply.github.com>
2023-12-17 18:42:17 +0200

    1

--- components/EditBtn.tsx
+++ components/EditBtn.tsx
@@ -1,7 +1,15 @@
 import Link from "next/link";
 import EditIcon from "./icons/EditIcon";
 
-export default function EditBtn({ topic }) {
+interface Topic {
+  _id: string;
+}
+
+interface EditBtnProps {
+  topic: Topic;
+}
+
+export default function EditBtn({ topic }: EditBtnProps) {
   return (
     <button className="btn btn-outline btn-warning flex">
       <Link href={`/editTopic/${topic._id}`}>
